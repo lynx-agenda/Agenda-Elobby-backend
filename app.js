ab5270d6dfe711419877595e94655fe869b17cdc
Francisco J. López <frandufour@protonmail.com>
2021-12-09 01:28:56 +0000

    add redis connection

--- app.js
+++ app.js
@@ -4,16 +4,26 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 const config = require("./config");
 
-const {
-  authenticateToken,
-  checkAuthUserValidity,
-} = require("./src/middlewares/auth.middleware");
+const Redis = require("redis");
+
+const client = Redis.createClient({
+	url: config.REDIS_URL,
+});
+
+client.on("error", (err) => console.log("Redis Client Error", err));
+client.on("connect", (err) => console.log("connected to redis successfully"));
+
+client.connect();
+
+module.exports = { client };
+
+const { authenticateToken, checkAuthUserValidity } = require("./src/middlewares/auth.middleware");
 
 // Create the Express application object
 const app = express();
 
 mongoose.connect(
-  `mongodb+srv://${config.DB_USERNAME}:${config.DB_PASSWORD}@cluster.ralzs.mongodb.net/${config.DB_NAME}?retryWrites=true&w=majority`
+	`mongodb+srv://${config.DB_USERNAME}:${config.DB_PASSWORD}@cluster.ralzs.mongodb.net/${config.DB_NAME}?retryWrites=true&w=majority`
 );
 
 //Load routings
@@ -37,14 +47,14 @@ app.use(authenticateToken);
 
 //Configure Header HTTP
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Authorization, X-API-KEY, X_Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Authorization, X-API-KEY, X_Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+	);
+	res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+	res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+	next();
 });
 
 // Routers Basic
@@ -56,12 +66,12 @@ app.use("/api/elements", elementsRouter);
 // app.use(`/api/${API_VERSION}`, authRoutes);
 
 app.listen(config.PORT, (err) => {
-  if (err) {
-    throw err;
-  } else {
-    console.log("###############");
-    console.log("### API REST ##");
-    console.log("###############");
-    console.log(`http://${config.HOST}:${config.PORT}/api/`);
-  }
+	if (err) {
+		throw err;
+	} else {
+		console.log("###############");
+		console.log("### API REST ##");
+		console.log("###############");
+		console.log(`http://${config.HOST}:${config.PORT}/api/`);
+	}
 });
